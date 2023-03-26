7e2098c74525e09c847876cf4ffe2be6ca8bf786
daavii-b <britodavi122@gmail.com>
2023-03-26 19:11:11 -0300

    feat: Alias Type

---
+++ src/AliasType/index.ts
@@ -0,0 +1,17 @@
+type Person = {
+    name: string;
+    age: number;
+    address: string;
+    email: string;
+}
+
+
+const newPerson: Person = {
+    name: 'Davi',
+    age: 12,
+    address: 'Testing',
+    email: 'britodavi'
+}
+
+
+console.log(newPerson);
